eae71a3264b7d97fee6a306f9e461e06b8b323dd
Jovanni D <superg3m@gmail.com>
2023-07-28 18:49:41 -0400

    not ideal

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Cookie Clicker Game</title>
+  <link rel="stylesheet" href="./style.css" />
+</head>
+
+<body>
+  <div class="titleContainer">
+    <h1>Cookie Clicker Game</h1>
+    <p>Click the cookie to earn points!</p>
+    <p>Points: <span id="score"><?php echo isset($_COOKIE['score']) ? $_COOKIE['score'] : 0; ?></span></p>
+  </div>
+  <div class="cookieContainer">
+    <button class="cookie-button" onclick="updateScore()"></button>
+  </div>
+
+  <script>
+    // Java Script Scope
+    // isset is used to see if a variable is null or not set for the most part
+    var score = <?php
+                if (isset($_COOKIE['score'])) {
+                  echo $_COOKIE['score'];
+                } else {
+                  echo 0;
+                }
+                ?>;
+    var scoreElement = document.getElementById('score');
+
+    function updateScore() {
+      score++;
+      scoreElement.textContent = score;
+
+      document.cookie = "score=" + score + "; path=/";
+    }
+  </script>
+</body>
+
+</html>
+
 <?php
-echo "hello"
+setcookie("score", 0, time() + (86400 * 1), "/");
 ?>
